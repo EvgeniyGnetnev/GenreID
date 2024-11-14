90a3119f3c0364913d0116d724495207a44bca8a
EvgeniyGnetnev <chaklittle@yandex.ru>
2024-11-14 23:10:34 +0300

    Добавил файл настройки сервера, конфигурационный файл и библиотеку для конфигурации - viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"github.com/EvgeniyGnetnev/GenreID"
+	"github.com/spf13/viper"
+	"log"
+)
 
 func main(){
+	if err := initConfig(); err != nil {
+		log.Fatalf("error intializing configs: %s", err.Error())
+	}
 
+	srv := new(genreid.Server)
+	if err := srv.Run(viper.GetString("port")); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
